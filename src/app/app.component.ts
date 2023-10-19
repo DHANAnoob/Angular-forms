f538a14a80a0f75ea9338c067363680f0231a42b
Dhanasekar <jdhanasekar1999@gmail.com>
2023-10-19 18:43:32 +0530

    changes in submit condition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,11 +21,14 @@ export class AppComponent implements OnInit {
       username: new FormControl(null),
       dob: new FormControl(null),
       gender: new FormControl('male'),
-      street: new FormControl(null,[Validators.required,Validators.minLength(10)]),
-      country: new FormControl('India'),
-      city: new FormControl(null),
-      region: new FormControl(null),
-      postal: new FormControl(null),
+  
+        street: new FormControl(null,[Validators.required,Validators.minLength(10)]),
+        country: new FormControl('India'),
+        city: new FormControl(null),
+        region: new FormControl(null),
+        postal: new FormControl(null),
+
+     
       skills: new FormControl(null),
     })
   }
