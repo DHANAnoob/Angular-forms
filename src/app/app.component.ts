c89020ceafb428a099aa76c423a61c1ee4d6e406
Dhanasekar <jdhanasekar1999@gmail.com>
2023-10-21 15:41:51 +0530

    custom validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CustomValidators } from './validators/noSpaceAllowed.validators'
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
@@ -15,8 +15,8 @@ export class AppComponent implements OnInit {
 
   ngOnInit(){
     this.reactiveForms = new FormGroup({
-      firstname: new FormControl(null, [Validators.required,Validators.minLength(2)]),
-      lastname: new FormControl(null, Validators.required),
+      firstname: new FormControl(null, [Validators.required,Validators.minLength(2),CustomValidators.noSpaceAllowed]),
+      lastname: new FormControl(null, [Validators.required, CustomValidators.noSpaceAllowed]),
       email: new FormControl(null , [Validators.email,Validators.required]),
       username: new FormControl(null),
       dob: new FormControl(null),
